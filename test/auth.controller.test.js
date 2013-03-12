ff7971a14f383a08451fcead735fe2249cdde10a
Atul Varma <varmaa@gmail.com>
2013-03-11 20:49:56 -0400

    factored appTest helper out of auth controller test

--- test/auth.controller.test.js
+++ test/auth.controller.test.js
@@ -1,89 +1,69 @@
-var test = require('tap').test;
-var createApp = require('../app');
-var http = require('http');
-var url = require('url');
-var request = require('request');
+var appTest = require('./index').appTest;
 
 var fakeOauth = {};
-var app = createApp({
-  cookieSecret: 'lol',
-  oauth: fakeOauth,
-  authenticateUrl: 'http://foo.org/auth'
-});
-var server = http.createServer(app);
-var req = request.defaults({
-  jar: request.jar(),
-  followRedirect: false
-});
-var resolve = function(path) {
-  return url.resolve('http://localhost:' + server.address().port, path);
-};
 
-test("(setup server)", function(t) {
-  server.listen(0, function(err) {
-    if (err) throw err;
-    t.end();
+appTest("/auth", {
+  appOptions: {
+    cookieSecret: 'lol',
+    oauth: fakeOauth,
+    authenticateUrl: 'http://foo.org/auth'
+  }
+}, function(t, a) {
+  t.test("/auth/info before login works", function(t) {
+    a.request(a.url('/auth/info'), function(err, response, body) {
+      t.equal(response.statusCode, 200);
+      t.same(JSON.parse(body), {screenName: null, userId: null});
+      t.end();
+    });
   });
-});
 
-test("/auth/info before login works", function(t) {
-  req(resolve('/auth/info'), function(err, response, body) {
-    t.equal(response.statusCode, 200);
-    t.same(JSON.parse(body), {screenName: null, userId: null});
-    t.end();
+  t.test("/auth/login works", function(t) {
+    fakeOauth.getOAuthRequestToken = function(cb) {
+      cb(null, "reqtoken", "reqsecret");
+    };
+    a.request(a.url('/auth/login'), function(err, response, body) {
+      if (err) throw err;
+      t.equal(response.statusCode, 302);
+      t.equal(response.headers['location'],
+              'http://foo.org/auth?oauth_token=reqtoken&force_login=true');
+      t.end();
+    });
   });
-});
 
-test("/auth/login works", function(t) {
-  fakeOauth.getOAuthRequestToken = function(cb) {
-    cb(null, "reqtoken", "reqsecret");
-  };
-  req(resolve('/auth/login'), function(err, response, body) {
-    if (err) throw err;
-    t.equal(response.statusCode, 302);
-    t.equal(response.headers['location'],
-            'http://foo.org/auth?oauth_token=reqtoken&force_login=true');
-    t.end();
+  t.test("/auth/callback works", function(t) {
+    fakeOauth.getOAuthAccessToken = function(token, secret, verifier, cb) {
+      t.equal(token, "reqtoken");
+      t.equal(secret, "reqsecret");
+      t.equal(verifier, "v2");
+      cb(null, "atoken", "asecret", {user_id: '12', screen_name: 'hi'});
+    };
+    a.request(a.url('/auth/callback?oauth_verifier=v2'),
+              function(err, res, body) {
+                t.equal(res.statusCode, 200);
+                t.equal(body, '<script>window.close();</script>');
+                t.end();
+              });
   });
-});
 
-test("/auth/callback works", function(t) {
-  fakeOauth.getOAuthAccessToken = function(token, secret, verifier, cb) {
-    t.equal(token, "reqtoken");
-    t.equal(secret, "reqsecret");
-    t.equal(verifier, "v2");
-    cb(null, "atoken", "asecret", {user_id: '12', screen_name: 'hi'});
-  };
-  req(resolve('/auth/callback?oauth_verifier=v2'), function(err, res, body) {
-    t.equal(res.statusCode, 200);
-    t.equal(body, '<script>window.close();</script>');
-    t.end();
+  t.test("/auth/info after login works", function(t) {
+    a.request(a.url('/auth/info'), function(err, response, body) {
+      if (err) throw err;
+      body = JSON.parse(body);
+      t.equal(response.statusCode, 200);
+      t.equal(typeof(body.userId), 'number');
+      t.same(body, {screenName: 'hi', userId: 12});
+      t.end();
+    });
   });
-});
 
-test("/auth/info after login works", function(t) {
-  req(resolve('/auth/info'), function(err, response, body) {
-    if (err) throw err;
-    body = JSON.parse(body);
-    t.equal(response.statusCode, 200);
-    t.equal(typeof(body.userId), 'number');
-    t.same(body, {screenName: 'hi', userId: 12});
-    t.end();
+  t.test("/auth/logout works", function(t) {
+    a.request.post(a.url('/auth/logout'), function(err, response, body) {
+      if (err) throw err;
+      t.equal(response.statusCode, 204);
+      t.ok(response.headers['set-cookie'][0].match(/connect\.sess=;/));
+      t.end();
+    });
   });
-});
 
-test("/auth/logout works", function(t) {
-  req.post(resolve('/auth/logout'), function(err, response, body) {
-    if (err) throw err;
-    t.equal(response.statusCode, 204);
-    t.ok(response.headers['set-cookie'][0].match(/connect\.sess=;/));
-    t.end();
-  });
-});
-
-test("(teardown server)", function(t) {
-  server.close(function(err) {
-    if (err) throw err;
-    t.end();
-  });
+  t.end();
 });
--- test/index.js
+++ test/index.js
@@ -1,7 +1,13 @@
+var http = require('http');
+var url = require('url');
 var mongoose = require('mongoose');
-var test = require('tap').test;
+var tap = require('tap');
+var request = require('request');
+var createApp = require('../app');
 
 var dbTest = exports.dbTest = function dbTest(name, cb) {
+  var test = tap.test.bind(tap);
+
   test("(setup db)", function(t) {
     var mongoUrl = process.env['MONGODB_TEST_URL'] ||
                    'mongodb://localhost/test';
@@ -22,4 +28,37 @@ var dbTest = exports.dbTest = function dbTest(name, cb) {
       t.end();
     });
   });
-}
+};
+
+var appTest = exports.appTest = function appTest(name, options, cb) {
+  var self = {};
+  var app = self.app = createApp(options.appOptions);
+  var server = self.server = http.createServer(app);
+  var test = options.t ? options.t.test.bind(options.t) : tap.test.bind(tap);
+
+  self.url = function(path) {
+    return url.resolve('http://localhost:' + server.address().port, path);
+  };
+  self.request = request.defaults({
+    jar: request.jar(),
+    followRedirect: false
+  });
+
+  test("(setup app)", function(t) {
+    server.listen(0, function(err) {
+      if (err) throw err;
+      t.end();
+    });
+  });
+
+  test(name, function(t) { cb(t, self); });
+
+  test("(teardown app)", function(t) {
+    server.close(function(err) {
+      if (err) throw err;
+      t.end();
+    });
+  });
+
+  if (options.t) options.t.end();
+};
