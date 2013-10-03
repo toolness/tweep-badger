9fe3adec534ee7d3be78ce9c93df8f830af840c1
Atul Varma <varmaa@gmail.com>
2013-10-03 16:15:46 -0400

    only re-render badge image when we know its appearance will change.

--- client/badge_image.js
+++ client/badge_image.js
@@ -1,5 +1,9 @@
 var BADGE_SIZE = 480;
 
+var getBadgeHash = function(badge) {
+  return JSON.stringify([badge.background]);
+};
+
 var renderBadge = function(holder, badge) {
   var canvas = Chibadge.build({
     size: BADGE_SIZE,
@@ -11,5 +15,9 @@ var renderBadge = function(holder, badge) {
 };
 
 Template.badgeImage.rendered = function() {
-  this._canvas = renderBadge(this.find('.canvasHolder'), this.data);
+  var hash = getBadgeHash(this.data);
+  if (this._lastHash != hash) {
+    this._lastHash = hash;
+    this._canvas = renderBadge(this.find('.canvasHolder'), this.data);
+  }
 };
